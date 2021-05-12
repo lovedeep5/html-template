42614d7799a22e4e9d87d61cade25b2812ba896a
lovedeep5 <lovedeep5.abh@gmail.com>
2021-05-12 23:13:28 +0530

    header added

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 
 <head>
     <title>Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php require_once './modules/head.php' ?>
 </head>
 
---
+++ modules/header.php
@@ -0,0 +1,14 @@
+<header class="header-1">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="header-company-logo">
+                    <a href="/" title="Website Logo" class="header-logo-link"><span class="logo">Werbrich’s Landscaping</span></a>
+                </div>
+            </div>
+            <div class="header-contact d-none d-md-block col-sm-4 col-md-6">
+                <span class="header-request-btn"><a class="btn-link" href="/contact/">Consultations</a></span><span class="header-phone"><a href="tel:+15136133694">(513) 613-3694</a></span>
+            </div>
+        </div>
+    </div>
+</header>
