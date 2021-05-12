b622df891c0f06c54d2d6693d948b2d7ef96583e
lovedeep5 <lovedeep5.abh@gmail.com>
2021-05-12 23:25:42 +0530

    Navbar added, Working On style

--- modules/head.php
+++ modules/head.php
@@ -1,4 +1,4 @@
-
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
 <link rel="stylesheet/less" type="text/css" href="./less/styles.less" />
--- modules/header.php
+++ modules/header.php
@@ -1,13 +1,13 @@
 <header class="header-1">
     <div class="container">
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-4">
                 <div class="header-company-logo">
                     <a href="/" title="Website Logo" class="header-logo-link"><span class="logo">Werbrich’s Landscaping</span></a>
                 </div>
             </div>
-            <div class="header-contact d-none d-md-block col-sm-4 col-md-6">
-                <span class="header-request-btn"><a class="btn-link" href="/contact/">Consultations</a></span><span class="header-phone"><a href="tel:+15136133694">(513) 613-3694</a></span>
+            <div class="header-contact d-none d-md-block col-md-8 text-end">
+                <span class="header-request-btn"><a class="btn-link" href="/contact/">Consultations</a></span><span class="header-phone"><a href="tel:+919876543210">(987) 654-3210</a></span>
             </div>
         </div>
     </div>
